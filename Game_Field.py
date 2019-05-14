c91e98ec9e0926fcbcef00439e988b2255b3dc98
KinoshitaYosuke <k1410159@gmail.com>
2019-05-14 22:53:16 +0900

    pythonファイルの追加

---
+++ Game_Field.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+
+class Player:
+    player_state
+    coordinate_x
+    coordinate_y
+    width
+    height
+    move_x
+    move_y
+    jump_counter
+    hit_info
+    
+    def initial_coordinate():
+    def set_coordinate():
+    def change_state(state):
+    def hit_check():
+    def jump_process():
+    def set_hit_info(info):
+
+    def get_x():
+    def get_y():
+    def get_w():
+    def get_h():
+    def get_mx():
+    def get_my():
+    def get_state():
+    def get_hit_info():
+    def get_jump_counter():
+
+def main():
+
+    return 0
+
+if __name__ == '__main__':
+    main()
